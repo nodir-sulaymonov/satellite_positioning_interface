7a9a4548519b3aac57ccff20d7a1c12517e2efbf
nodirbeksulaymonov <s.nodir9494@gmail.com>
2021-02-10 01:12:04 +0300

    fill in InputField

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -7,7 +7,8 @@ const BasicExample = () => (
     <div>
         <h1>My Form</h1>
         <Formik
-            initialValues={{ name: 'jared' }}
+            initialValues={{ name: 'jared',
+                             category: '23'   }}
             onSubmit={(values, actions) => {
                 setTimeout(() => {
                     alert(JSON.stringify(values, null, 2));
@@ -24,7 +25,15 @@ const BasicExample = () => (
                         value={props.values.name}
                         name="name"
                     />
-                    {props.errors.name && <div className="feedback">{props.errors.name}</div>}
+                    {props.errors.name && <div className="inputField">{props.errors.name}</div>}
+                    <input
+                        type="text"
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        value={props.values.category}
+                        name="name"
+                    />
+                    {props.errors.category && <div className="inputField">{props.errors.category}</div>}
                     <button type="submit">Submit</button>
                 </form>
             )}
